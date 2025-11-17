83d72178dfe8762ce54348dd0c7f01accaca4148
Jack <farrelljack741@gmail.com>
2025-11-17 23:21:52 +0000

    Final Commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,22 @@
 /**
  * Simple Homepage consisting of two components
  * When displayed on a smaller device, the Design component is displayed under the Banner
+ * Used lazy loading to load the large banner to improve overall performance
  */
+import React, { Suspense, lazy } from 'react';
 
-import Banner from '../components/Banner'
+const Banner = lazy(() => import('../components/Banner'));
+
 import Design from '../components/Design'
 
 const Home = () => {
   return (
     <div className='flex h-100 w-screen'>
       <div className='flex md:flex-row mt-10 flex-col w-full'>
-        <Banner />
+        <Suspense fallback={<div className="text-center p-4">Loading Banner...</div>}>
+          <Banner />
+        </Suspense>
+
         <Design/>
       </div>
     </div>
